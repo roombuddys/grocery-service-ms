c57687f7502168dbda660d38cece6c9b7ea34d89
vijay raju <gvijay222@gmail.com>
2019-12-07 17:15:35 +0530

    spring boot project setup code added

---
+++ src/test/java/com/vm/groceryservicems/GroceryServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.groceryservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
